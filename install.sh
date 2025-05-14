708d67dc4bc42db8017d1ee35cc5fd7fd8b12ece
SchoolGoBye <wood_pulley_6c@icloud.com>
2025-05-14 11:15:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-git clone https://github.com/Blobby-Boi/BlobeVM
-cd BlobeVM
+git clone https://github.com/SchoolGoBye/DylanVM/
+cd DylanVM
 pip install textual
 sleep 2
 python3 installer.py
-docker build -t blobevm . --no-cache
+docker build -t dylanvm . --no-cache
 cd ..
 
 sudo apt update
 sudo apt install -y jq
 
 mkdir Save
-cp -r BlobeVM/root/config/* Save
+cp -r DylanVM/root/config/* Save
 
-json_file="BlobeVM/options.json"
+json_file="DylanVM/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=DylanVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=DylanVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped dylanvm
 else
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=DylanVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=DylanVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped dylanvm
 fi
 clear
-echo "BLOBEVM WAS INSTALLED SUCCESSFULLY! Check Port Tab"
+echo "DYLANVM WAS INSTALLED SUCCESSFULLY! Check Port Tab tp see if this GameFlow Invention works!"
